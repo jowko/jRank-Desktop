7a04679c109a051e5ea00bdfae52c66ac5341339
Piotr Jówko <piotr.jowko@impaqgroup.com>
2018-04-17 18:07:41 +0200

    Create install.bat script for creating JRank application

---
+++ install.bat
@@ -0,0 +1,24 @@
+REM Script for creating jrank installation
+
+@echo off
+
+echo Building whole project
+call mvn clean install
+
+echo Removing old JRank files
+if exist JRank\ del /s /f /q JRank\*.*
+
+echo Creating JRank folder
+md JRank
+
+echo Creating default workspace folder
+md JRank\workspace
+
+echo Copying and renaming main jar file
+COPY desktop-app\target\desktop-app-0.1-SNAPSHOT-jar-with-dependencies.jar "JRank\JRank-0.1-SNAPSHOT.jar"
+
+echo copying data directory
+ROBOCOPY data "JRank\data"
+
+echo creating run.bat file
+@echo java -jar JRank-0.1-SNAPSHOT.jar -Xms128m -Xmx4G > JRank\run.bat
