ee26a7edfe4fff5d5a54e2a13f9424c0b5737e7e
Piotr Jówko <piotr.jowko@impaqgroup.com>
2018-04-21 17:21:43 +0200

    Commit missing class BooleanUtils

---
+++ desktop-app/src/main/java/pl/jowko/jrank/desktop/utils/BooleanUtils.java
@@ -0,0 +1,12 @@
+package pl.jowko.jrank.desktop.utils;
+
+/**
+ * Created by Piotr on 2018-04-18.
+ */
+public class BooleanUtils {
+	
+	public static boolean not(boolean condition) {
+		return !condition;
+	}
+	
+}
