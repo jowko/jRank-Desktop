5c9979f3dd781819cc5d6c5ac6d30a374123adc2
Piotr Jówko <piotr.jowko@impaqgroup.com>
2018-04-21 15:38:43 +0200

    Add example experiments to install.bat

--- install.bat
+++ install.bat
@@ -17,8 +17,16 @@ md JRank\workspace
 echo Copying and renaming main jar file
 COPY desktop-app\target\desktop-app-0.1-SNAPSHOT-jar-with-dependencies.jar "JRank\JRank-0.1-SNAPSHOT.jar"
 
-echo copying data directory
+echo Copying data directory
 ROBOCOPY data "JRank\data"
+
+echo Copying example experiments
+
+echo Copying Houses7
+ROBOCOPY workspace\Houses7 JRank\workspace\Houses7
+
+echo Copying Houses11
+ROBOCOPY workspace\Houses11 JRank\workspace\Houses11
 
 echo creating run.bat file
 @echo java -jar JRank-0.1-SNAPSHOT.jar -Xms128m -Xmx4G > JRank\run.bat
