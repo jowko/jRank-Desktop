fbaf0f525f0674bafdb2d8f5987d3643fe245420
Piotr Jówko <piotr.jowko@impaqgroup.com>
2018-05-04 17:22:49 +0200

    Introduce PropertiesControllerHelper for handling actions from controller. Create new action - Clear form

--- desktop-app/src/main/java/pl/jowko/jrank/desktop/feature/properties/PropertiesController.java
+++ desktop-app/src/main/java/pl/jowko/jrank/desktop/feature/properties/PropertiesController.java
@@ -4,165 +4,90 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import pl.jowko.jrank.desktop.utils.Cloner;
 
-import static java.util.Objects.isNull;
-
 /**
  * Created by Piotr on 2018-04-29.
  */
 public class PropertiesController {
 	
-	@FXML private Label pctFileLabel;
-	@FXML private TextField pctFile;
-	@FXML private Label pctApxFileLabel;
-	@FXML private TextField pctApxFile;
-	@FXML private Label pctRulesFileLabel;
-	@FXML private TextField pctRulesFile;
-	@FXML private Label graphFileLabel;
-	@FXML private TextField graphFile;
-	@FXML private Label rankingFileLabel;
-	@FXML private TextField rankingFile;
-	@FXML private Label learningDataFileLabel;
-	@FXML private TextField learningDataFile;
-	@FXML private Label testDataFileLabel;
-	@FXML private TextField testDataFile;
+	@FXML Label pctFileLabel;
+	@FXML TextField pctFile;
+	@FXML Label pctApxFileLabel;
+	@FXML TextField pctApxFile;
+	@FXML Label pctRulesFileLabel;
+	@FXML TextField pctRulesFile;
+	@FXML Label graphFileLabel;
+	@FXML TextField graphFile;
+	@FXML Label rankingFileLabel;
+	@FXML TextField rankingFile;
+	@FXML Label learningDataFileLabel;
+	@FXML TextField learningDataFile;
+	@FXML Label testDataFileLabel;
+	@FXML TextField testDataFile;
 	
-	@FXML private Label referenceRankingLabel;
-	@FXML private TextField referenceRanking;
-	@FXML private Label pairsLabel;
-	@FXML private TextField pairs;
+	@FXML Label referenceRankingLabel;
+	@FXML TextField referenceRanking;
+	@FXML Label pairsLabel;
+	@FXML TextField pairs;
 	
-	@FXML private Label typeOfFamilyCriteriaLabel;
-	@FXML private ComboBox<JRankParameter> typeOfFamilyCriteria;
-	@FXML private Label typeOfRulesLabel;
-	@FXML private ComboBox<JRankParameter> typeOfRules;
-	@FXML private Label consideredSetOfRulesLabel;
-	@FXML private ComboBox<JRankParameter> consideredSetOfRules;
+	@FXML Label typeOfFamilyCriteriaLabel;
+	@FXML ComboBox<JRankParameter> typeOfFamilyCriteria;
+	@FXML Label typeOfRulesLabel;
+	@FXML ComboBox<JRankParameter> typeOfRules;
+	@FXML Label consideredSetOfRulesLabel;
+	@FXML ComboBox<JRankParameter> consideredSetOfRules;
 	
-	@FXML private Label consistencyMeasureLabel;
-	@FXML private ComboBox<JRankParameter> consistencyMeasure;
-	@FXML private Label consistencyMeasureThresholdLabel;
-	@FXML private TextField consistencyMeasureThreshold;
-	@FXML private Label precisionLabel;
-	@FXML private TextField precision;
+	@FXML Label consistencyMeasureLabel;
+	@FXML ComboBox<JRankParameter> consistencyMeasure;
+	@FXML Label consistencyMeasureThresholdLabel;
+	@FXML TextField consistencyMeasureThreshold;
+	@FXML Label precisionLabel;
+	@FXML TextField precision;
 	
-	@FXML private Label rankingProcedureLabel;
-	@FXML private ComboBox<JRankParameter> rankingProcedure;
-	@FXML private Label dominanceLabel;
-	@FXML private ComboBox<JRankParameter> dominance;
-	@FXML private Label dominanceForPairsLabel;
-	@FXML private ComboBox<JRankParameter> dominanceForPairs;
+	@FXML Label rankingProcedureLabel;
+	@FXML ComboBox<JRankParameter> rankingProcedure;
+	@FXML Label dominanceLabel;
+	@FXML ComboBox<JRankParameter> dominance;
+	@FXML Label dominanceForPairsLabel;
+	@FXML ComboBox<JRankParameter> dominanceForPairs;
 	
-	@FXML private Label satisfactionDegreesInGraphLabel;
-	@FXML private ComboBox<JRankParameter> satisfactionDegreesInGraph;
-	@FXML private Label fuzzyCalculationMethodLabel;
-	@FXML private ComboBox<JRankParameter> fuzzyCalculationMethod;
+	@FXML Label satisfactionDegreesInGraphLabel;
+	@FXML ComboBox<JRankParameter> satisfactionDegreesInGraph;
+	@FXML Label fuzzyCalculationMethodLabel;
+	@FXML ComboBox<JRankParameter> fuzzyCalculationMethod;
 	
-	@FXML private Label negativeExamplesTreatmentLabel;
-	@FXML private ComboBox<JRankParameter> negativeExamplesTreatment;
-	@FXML private Label optimizeRuleConsistencyLabel;
-	@FXML private ComboBox<JRankParameter> optimizeRuleConsistency;
-	@FXML private Label ruleConditionsSelectionMethodLabel;
-	@FXML private ComboBox<JRankParameter> ruleConditionsSelectionMethod;
+	@FXML Label negativeExamplesTreatmentLabel;
+	@FXML ComboBox<JRankParameter> negativeExamplesTreatment;
+	@FXML Label optimizeRuleConsistencyLabel;
+	@FXML ComboBox<JRankParameter> optimizeRuleConsistency;
+	@FXML Label ruleConditionsSelectionMethodLabel;
+	@FXML ComboBox<JRankParameter> ruleConditionsSelectionMethod;
 	
-	@FXML private CheckBox allowEmptyRules;
-	@FXML private CheckBox useEdgeRegions;
+	@FXML CheckBox allowEmptyRules;
+	@FXML CheckBox useEdgeRegions;
 	
-	@FXML private CheckBox writeDominationInformation;
-	@FXML private CheckBox writeRulesStatistics;
-	@FXML private CheckBox writeLearningPositiveExamples;
+	@FXML CheckBox writeDominationInformation;
+	@FXML CheckBox writeRulesStatistics;
+	@FXML CheckBox writeLearningPositiveExamples;
 	
-	@FXML private Button saveButton;
-	@FXML private Button cancelButton;
-	@FXML private Button setDefaultsButton;
+	@FXML Button saveButton;
+	@FXML Button cancelButton;
+	@FXML Button setDefaultsButton;
+	@FXML Button clearButton;
 	
-	private JRankParametersService parametersService;
-	private JRankParameter emptyParameter;
-	private JRankProperties properties;
-	private JRankProperties editableProperties;
-
-	@FXML
-	public void initialize() {
-		parametersService = JRankParametersService.getInstance();
-		emptyParameter = parametersService.getEmptyParameter();
-	}
+	private PropertiesControllerHelper controllerHelper;
+	JRankProperties properties;
+	JRankProperties editableProperties;
 	
 	public void initializeProperties(JRankProperties properties) {
 		this.properties = properties;
 		editableProperties = (JRankProperties) Cloner.deepClone(properties);
-		fillComboBoxes();
-		initializeFieldsValues();
+		controllerHelper = new PropertiesControllerHelper(this);
+		controllerHelper.fillComboBoxes();
+		controllerHelper.fillFieldsValues();
 	}
 	
-	private void fillComboBoxes() {
-		typeOfFamilyCriteria.getItems().addAll(parametersService.getTypeOfFamilyOfCriteria());
-		typeOfRules.getItems().addAll(parametersService.getTypeOfRules());
-		consideredSetOfRules.getItems().addAll(parametersService.getConsideredSetOfRules());
-		
-		consistencyMeasure.getItems().addAll(parametersService.getConsistencyMeasure());
-		
-		rankingProcedure.getItems().addAll(parametersService.getRankingProcedure());
-		dominance.getItems().addAll(parametersService.getDominance());
-		dominanceForPairs.getItems().addAll(parametersService.getDominanceForPairsOfOrdinalValues());
-		
-		satisfactionDegreesInGraph.getItems().addAll(parametersService.getSatisfactionDegreesInPreferenceGraph());
-		fuzzyCalculationMethod.getItems().addAll(parametersService.getFuzzySatisfactionDegreeCalculationMethod());
-		
-		negativeExamplesTreatment.getItems().addAll(parametersService.getNegativeExamplesTreatmentForVCDRSA());
-		optimizeRuleConsistency.getItems().addAll(parametersService.getOptimizeRuleConsistencyInVCDomLEMWrt());
-		ruleConditionsSelectionMethod.getItems().addAll(parametersService.getRuleConditionsSelectionMethodInVCDomLEM());
-	}
-	
-	private void initializeFieldsValues() {
-		learningDataFile.setText(editableProperties.getLearningDataFile());
-		testDataFile.setText(editableProperties.getTestDataFile());
-		pctFile.setText(editableProperties.getPctFile());
-		pctApxFile.setText(editableProperties.getPctApxFile());
-		pctRulesFile.setText(editableProperties.getPctRulesFile());
-		graphFile.setText(editableProperties.getPreferenceGraphFile());
-		rankingFile.setText(editableProperties.getRankingFile());
-		
-		referenceRanking.setText(editableProperties.getReferenceRanking());
-		pairs.setText(editableProperties.getPairs());
-		
-		typeOfFamilyCriteria.getSelectionModel().select(editableProperties.getTypeOfFamilyOfCriteria());
-		typeOfRules.getSelectionModel().select(editableProperties.getTypeOfRules());
-		consideredSetOfRules.getSelectionModel().select(editableProperties.getConsideredSetOfRules());
-		
-		consistencyMeasure.getSelectionModel().select(editableProperties.getConsistencyMeasure());
-		consistencyMeasureThreshold.setText(getStringOrNull(editableProperties.getConsistencyMeasureThreshold()));
-		precision.setText(getStringOrNull(editableProperties.getPrecision()));
-		
-		rankingProcedure.getSelectionModel().select(editableProperties.getRankingProcedure());
-		dominance.getSelectionModel().select(editableProperties.getDominance());
-		dominanceForPairs.getSelectionModel().select(editableProperties.getDominanceForPairsOfOrdinalValues());
-		
-		satisfactionDegreesInGraph.getSelectionModel().select(editableProperties.getSatisfactionDegreesInPreferenceGraph());
-		fuzzyCalculationMethod.getSelectionModel().select(editableProperties.getFuzzySatisfactionDegreeCalculationMethod());
-		
-		negativeExamplesTreatment.getSelectionModel().select(editableProperties.getNegativeExamplesTreatmentForVCDRSA());
-		optimizeRuleConsistency.getSelectionModel().select(editableProperties.getOptimizeRuleConsistencyInVCDomLEMWrt());
-		ruleConditionsSelectionMethod.getSelectionModel().select(editableProperties.getRuleConditionsSelectionMethodInVCDomLEM());
-		
-		allowEmptyRules.setSelected(getBoolean(editableProperties.getAllowEmptyRulesInVCDomLEM()));
-		useEdgeRegions.setSelected(getBoolean(editableProperties.getUseEdgeRegionsInVCDomLEM()));
-		
-		writeDominationInformation.setSelected(getBoolean(editableProperties.getWriteDominationInformation()));
-		writeRulesStatistics.setSelected(getBoolean(editableProperties.getWriteRulesStatistics()));
-		writeLearningPositiveExamples.setSelected(getBoolean(editableProperties.getWriteLearningPositiveExamples()));
-	}
-
-	private String getStringOrNull(Object value) {
-		if(isNull(value)) {
-			return null;
-		}
-		return value.toString();
-	}
-	
-	private boolean getBoolean(Boolean value) {
-		if(isNull(value)) {
-			return false;
-		}
-		return value;
+	public void clearFormAction() {
+		controllerHelper.clearForm();
 	}
 	
 }
---
+++ desktop-app/src/main/java/pl/jowko/jrank/desktop/feature/properties/PropertiesControllerHelper.java
@@ -0,0 +1,135 @@
+package pl.jowko.jrank.desktop.feature.properties;
+
+import static java.util.Objects.isNull;
+
+/**
+ * Created by Piotr on 2018-05-04.
+ */
+class PropertiesControllerHelper {
+	
+	private JRankParametersService parametersService;
+	private PropertiesController controller;
+	private JRankProperties editableProperties;
+	
+	PropertiesControllerHelper(PropertiesController controller) {
+		parametersService = JRankParametersService.getInstance();
+		this.controller = controller;
+		editableProperties = controller.editableProperties;
+	}
+	
+	void fillComboBoxes() {
+		controller.typeOfFamilyCriteria.getItems().addAll(parametersService.getTypeOfFamilyOfCriteria());
+		controller.typeOfRules.getItems().addAll(parametersService.getTypeOfRules());
+		controller.consideredSetOfRules.getItems().addAll(parametersService.getConsideredSetOfRules());
+		
+		controller.consistencyMeasure.getItems().addAll(parametersService.getConsistencyMeasure());
+		
+		controller.rankingProcedure.getItems().addAll(parametersService.getRankingProcedure());
+		controller.dominance.getItems().addAll(parametersService.getDominance());
+		controller.dominanceForPairs.getItems().addAll(parametersService.getDominanceForPairsOfOrdinalValues());
+		
+		controller.satisfactionDegreesInGraph.getItems().addAll(parametersService.getSatisfactionDegreesInPreferenceGraph());
+		controller.fuzzyCalculationMethod.getItems().addAll(parametersService.getFuzzySatisfactionDegreeCalculationMethod());
+		
+		controller.negativeExamplesTreatment.getItems().addAll(parametersService.getNegativeExamplesTreatmentForVCDRSA());
+		controller.optimizeRuleConsistency.getItems().addAll(parametersService.getOptimizeRuleConsistencyInVCDomLEMWrt());
+		controller.ruleConditionsSelectionMethod.getItems().addAll(parametersService.getRuleConditionsSelectionMethodInVCDomLEM());
+	}
+	
+	void clearForm() {
+		JRankParameter emptyParameter = parametersService.getEmptyParameter();
+		
+		editableProperties.setLearningDataFile(null);
+		editableProperties.setTestDataFile(null);
+		editableProperties.setPctFile(null);
+		editableProperties.setPctApxFile(null);
+		editableProperties.setPctRulesFile(null);
+		editableProperties.setPreferenceGraphFile(null);
+		editableProperties.setRankingFile(null);
+		
+		editableProperties.setReferenceRanking(null);
+		editableProperties.setPairs(null);
+		
+		editableProperties.setTypeOfFamilyOfCriteria(emptyParameter);
+		editableProperties.setTypeOfRules(emptyParameter);
+		editableProperties.setConsideredSetOfRules(emptyParameter);
+		
+		editableProperties.setConsistencyMeasure(emptyParameter);
+		editableProperties.setConsistencyMeasureThreshold(null);
+		editableProperties.setPrecision(null);
+		
+		editableProperties.setRankingProcedure(emptyParameter);
+		editableProperties.setDominance(emptyParameter);
+		editableProperties.setDominanceForPairsOfOrdinalValues(emptyParameter);
+		
+		editableProperties.setSatisfactionDegreesInPreferenceGraph(emptyParameter);
+		editableProperties.setFuzzySatisfactionDegreeCalculationMethod(emptyParameter);
+		
+		editableProperties.setNegativeExamplesTreatmentForVCDRSA(emptyParameter);
+		editableProperties.setOptimizeRuleConsistencyInVCDomLEMWrt(emptyParameter);
+		editableProperties.setRuleConditionsSelectionMethodInVCDomLEM(emptyParameter);
+		
+		editableProperties.setAllowEmptyRulesInVCDomLEM(null);
+		editableProperties.setUseEdgeRegionsInVCDomLEM(null);
+		
+		editableProperties.setWriteDominationInformation(null);
+		editableProperties.setWriteRulesStatistics(null);
+		editableProperties.setWriteLearningPositiveExamples(null);
+		
+		fillFieldsValues();
+	}
+	
+	void fillFieldsValues() {
+		controller.learningDataFile.setText(editableProperties.getLearningDataFile());
+		controller.testDataFile.setText(editableProperties.getTestDataFile());
+		controller.pctFile.setText(editableProperties.getPctFile());
+		controller.pctApxFile.setText(editableProperties.getPctApxFile());
+		controller.pctRulesFile.setText(editableProperties.getPctRulesFile());
+		controller.graphFile.setText(editableProperties.getPreferenceGraphFile());
+		controller.rankingFile.setText(editableProperties.getRankingFile());
+		
+		controller.referenceRanking.setText(editableProperties.getReferenceRanking());
+		controller.pairs.setText(editableProperties.getPairs());
+		
+		controller.typeOfFamilyCriteria.getSelectionModel().select(editableProperties.getTypeOfFamilyOfCriteria());
+		controller.typeOfRules.getSelectionModel().select(editableProperties.getTypeOfRules());
+		controller.consideredSetOfRules.getSelectionModel().select(editableProperties.getConsideredSetOfRules());
+		
+		controller.consistencyMeasure.getSelectionModel().select(editableProperties.getConsistencyMeasure());
+		controller.consistencyMeasureThreshold.setText(getStringOrNull(editableProperties.getConsistencyMeasureThreshold()));
+		controller.precision.setText(getStringOrNull(editableProperties.getPrecision()));
+		
+		controller.rankingProcedure.getSelectionModel().select(editableProperties.getRankingProcedure());
+		controller.dominance.getSelectionModel().select(editableProperties.getDominance());
+		controller.dominanceForPairs.getSelectionModel().select(editableProperties.getDominanceForPairsOfOrdinalValues());
+		
+		controller.satisfactionDegreesInGraph.getSelectionModel().select(editableProperties.getSatisfactionDegreesInPreferenceGraph());
+		controller.fuzzyCalculationMethod.getSelectionModel().select(editableProperties.getFuzzySatisfactionDegreeCalculationMethod());
+		
+		controller.negativeExamplesTreatment.getSelectionModel().select(editableProperties.getNegativeExamplesTreatmentForVCDRSA());
+		controller.optimizeRuleConsistency.getSelectionModel().select(editableProperties.getOptimizeRuleConsistencyInVCDomLEMWrt());
+		controller.ruleConditionsSelectionMethod.getSelectionModel().select(editableProperties.getRuleConditionsSelectionMethodInVCDomLEM());
+		
+		controller.allowEmptyRules.setSelected(getBoolean(editableProperties.getAllowEmptyRulesInVCDomLEM()));
+		controller.useEdgeRegions.setSelected(getBoolean(editableProperties.getUseEdgeRegionsInVCDomLEM()));
+		
+		controller.writeDominationInformation.setSelected(getBoolean(editableProperties.getWriteDominationInformation()));
+		controller.writeRulesStatistics.setSelected(getBoolean(editableProperties.getWriteRulesStatistics()));
+		controller.writeLearningPositiveExamples.setSelected(getBoolean(editableProperties.getWriteLearningPositiveExamples()));
+	}
+	
+	private String getStringOrNull(Object value) {
+		if(isNull(value)) {
+			return null;
+		}
+		return value.toString();
+	}
+	
+	private boolean getBoolean(Boolean value) {
+		if(isNull(value)) {
+			return false;
+		}
+		return value;
+	}
+	
+}
