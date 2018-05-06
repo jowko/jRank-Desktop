165aa866789e59347d2fdbc3472395348db0d8c2
Piotr Jówko <piotr.jowko@impaqgroup.com>
2018-05-06 19:54:01 +0200

    Precision and consistencyMeasureThreshold are now number fields

--- desktop-app/src/main/java/module-info.java
+++ desktop-app/src/main/java/module-info.java
@@ -12,6 +12,7 @@ module jRank.desktop {
 	requires jRS;
 	
 	opens pl.jowko.jrank.desktop.controller;
+	opens pl.jowko.jrank.desktop.feature.javafx;
 	opens pl.jowko.jrank.desktop.feature.properties;
 	opens pl.jowko.jrank.desktop.feature.upperTabs;
 	opens pl.jowko.jrank.desktop.feature.workspace;
