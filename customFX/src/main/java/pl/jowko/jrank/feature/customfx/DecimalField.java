cae478691ddede2b50b839a2b5733783cb2d3a65
Piotr Jówko <piotr.jowko@impaqgroup.com>
2018-05-09 17:46:15 +0200

    Refactor number fields - add NumberField class

--- customFX/src/main/java/pl/jowko/jrank/feature/customfx/DecimalField.java
+++ customFX/src/main/java/pl/jowko/jrank/feature/customfx/DecimalField.java
@@ -1,40 +1,20 @@
 package pl.jowko.jrank.feature.customfx;
 
-import javafx.scene.control.TextField;
-
-import java.util.regex.Pattern;
-
-import static java.util.Objects.isNull;
-
 /**
  * Created by Piotr on 2018-05-06.
  * This TextField accepts decimal numbers on input.
  */
-public class DecimalField extends TextField {
+public class DecimalField extends NumberField {
 	
-	private static Pattern decimalPattern = Pattern.compile("[0-9]*(\\.[0-9]*)?");
+	public static final String POSITIVE_DECIMAL_PATTERN = "[0-9]*(\\.[0-9]*)?";
+	public static final String NEGATIVE_DECIMAL_PATTERN = "[-][0-9]*(\\.[0-9]*)?";
 	
-	@Override
-	public void replaceText(int start, int end, String text) {
-		if (validate(start, text)) {
-			super.replaceText(start, end, text);
-		}
+	public DecimalField() {
+		super(POSITIVE_DECIMAL_PATTERN);
 	}
 	
-	@Override
-	public void replaceSelection(String text) {
-		if (validate(Integer.MAX_VALUE, text)) {
-			super.replaceSelection(text);
-		}
-	}
-	
-	private boolean validate(int start, String text) {
-		String currentText = isNull(getText()) ? "" : getText();
-		if(start == 0) {
-			return decimalPattern.matcher(text + currentText).matches();
-		} else {
-			return decimalPattern.matcher(currentText + text).matches();
-		}
+	public DecimalField(String pattern) {
+		super(pattern);
 	}
 	
 }
--- customFX/src/main/java/pl/jowko/jrank/feature/customfx/IntegerField.java
+++ customFX/src/main/java/pl/jowko/jrank/feature/customfx/IntegerField.java
@@ -1,40 +1,20 @@
 package pl.jowko.jrank.feature.customfx;
 
-import javafx.scene.control.TextField;
-
-import java.util.regex.Pattern;
-
-import static java.util.Objects.isNull;
-
 /**
  * Created by Piotr on 2018-05-06.
  * This TextField accepts only integers on input.
  */
-public class IntegerField extends TextField {
+public class IntegerField extends NumberField {
 	
-	private static Pattern integerPattern = Pattern.compile("[-]*[0-9]*");
+	public static final String POSITIVE_INTEGER_PATTERN = "[0-9]*";
+	public static final String NEGATIVE_INTEGER_PATTERN = "[-]?[0-9]*";
 	
-	@Override
-	public void replaceText(int start, int end, String text) {
-		if (validate(start, text)) {
-			super.replaceText(start, end, text);
-		}
+	public IntegerField() {
+		super(NEGATIVE_INTEGER_PATTERN);
 	}
 	
-	@Override
-	public void replaceSelection(String text) {
-		if (validate(Integer.MAX_VALUE, text)) {
-			super.replaceSelection(text);
-		}
-	}
-	
-	private boolean validate(int start, String text) {
-		String currentText = isNull(getText()) ? "" : getText();
-		if(start == 0) {
-			return integerPattern.matcher(text + currentText).matches();
-		} else {
-			return integerPattern.matcher(currentText + text).matches();
-		}
+	public IntegerField(String pattern) {
+		super(pattern);
 	}
 	
 }
