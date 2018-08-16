9fd58b506d9e6871869c043367fbe6bfd26925d9
Piotr Jówko <piotr.jowko@gmail.com>
2018-08-16 19:31:37 +0200

    RuleRankParametersService - replace ArrayList with LinkedList

--- desktop-app/src/main/java/pl/jowko/rulerank/desktop/feature/properties/RuleRankParametersService.java
+++ desktop-app/src/main/java/pl/jowko/rulerank/desktop/feature/properties/RuleRankParametersService.java
@@ -3,7 +3,7 @@ package pl.jowko.rulerank.desktop.feature.properties;
 import pl.jowko.rulerank.desktop.feature.internationalization.Labels;
 import pl.jowko.rulerank.desktop.feature.internationalization.LanguageService;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import static pl.poznan.put.cs.idss.jrs.Settings.ANY_FAMILY_OF_CRITERIA;
@@ -148,14 +148,14 @@ class RuleRankParametersService {
 	}
 	
 	private void initTypeOfFamilyOfCriteria() {
-		typeOfFamilyOfCriteria = new ArrayList<>();
+		typeOfFamilyOfCriteria = new LinkedList<>();
 		typeOfFamilyOfCriteria.add(getDefaultParameter());
 		typeOfFamilyOfCriteria.add(new RuleRankParameter(get(Labels.ANY_FAMILY_OF_CRITERIA),"any", ANY_FAMILY_OF_CRITERIA));
 		typeOfFamilyOfCriteria.add(new RuleRankParameter(get(Labels.CONSISTENT_FAMILY_OF_CRITERIA),"consistent", CONSISTENT_FAMILY_OF_CRITERIA));
 	}
 	
 	private void initConsistencyMeasure() {
-		consistencyMeasure = new ArrayList<>();
+		consistencyMeasure = new LinkedList<>();
 		consistencyMeasure.add(getDefaultParameter());
 		consistencyMeasure.add(new RuleRankParameter(get(Labels.EPSILON),"epsilon", EPSILON));
 		consistencyMeasure.add(new RuleRankParameter(get(Labels.EPSILON_STAR),"epsilon*", EPSILON_STAR));
@@ -164,35 +164,35 @@ class RuleRankParametersService {
 	}
 	
 	private void initTypeOfRules() {
-		typeOfRules = new ArrayList<>();
+		typeOfRules = new LinkedList<>();
 		typeOfRules.add(getDefaultParameter());
 		typeOfRules.add(new RuleRankParameter(get(Labels.RULE_CERTAIN),"certain", CERTAIN));
 		typeOfRules.add(new RuleRankParameter(get(Labels.RULE_POSSIBLE),"possible", POSSIBLE));
 	}
 	
 	private void initConsideredSetOfRules() {
-		consideredSetOfRules = new ArrayList<>();
+		consideredSetOfRules = new LinkedList<>();
 		consideredSetOfRules.add(getDefaultParameter());
 		consideredSetOfRules.add(new RuleRankParameter(get(Labels.MINIMAL_SET_OF_RULES),"minimal", MINIMAL_SET_OF_RULES));
 		consideredSetOfRules.add(new RuleRankParameter(get(Labels.EXHAUSTIVE_SET_OF_RULES),"exhaustive", EXHAUSTIVE_SET_OF_RULES));
 	}
 	
 	private void initSatisfactionDegreesInPreferenceGraph() {
-		satisfactionDegreesInPreferenceGraph = new ArrayList<>();
+		satisfactionDegreesInPreferenceGraph = new LinkedList<>();
 		satisfactionDegreesInPreferenceGraph.add(getDefaultParameter());
 		satisfactionDegreesInPreferenceGraph.add(new RuleRankParameter(get(Labels.SATISFACTION_FUZZY),"fuzzy", FUZZY));
 		satisfactionDegreesInPreferenceGraph.add(new RuleRankParameter(get(Labels.SATISFACTION_CRISP),"crisp", CRISP));
 	}
 	
 	private void initFuzzySatisfactionDegreeCalculationMethod() {
-		fuzzySatisfactionDegreeCalculationMethod = new ArrayList<>();
+		fuzzySatisfactionDegreeCalculationMethod = new LinkedList<>();
 		fuzzySatisfactionDegreeCalculationMethod.add(getDefaultParameter());
 		fuzzySatisfactionDegreeCalculationMethod.add(new RuleRankParameter(get(Labels.MAX_CREDIBILITY),"max-credibility", MAX_CREDIBILITY));
 		fuzzySatisfactionDegreeCalculationMethod.add(new RuleRankParameter(get(Labels.MAX_CREDIBILITY_X_FACTOR),"max-credibility-x-coverage-factor", MAX_PRODUCT_OF_CREDIBILITY_AND_COVERAGE_FACTOR));
 	}
 	
 	private void initRankingProcedure() {
-		rankingProcedure = new ArrayList<>();
+		rankingProcedure = new LinkedList<>();
 		rankingProcedure.add(getDefaultParameter());
 		rankingProcedure.add(new RuleRankParameter(get(Labels.NFS),"nfs", NFS));
 		rankingProcedure.add(new RuleRankParameter(get(Labels.REPEATED_NFS),"rnfs", REPEATED_NFS));
@@ -203,21 +203,21 @@ class RuleRankParametersService {
 	}
 	
 	private void initDominance() {
-		dominance = new ArrayList<>();
+		dominance = new LinkedList<>();
 		dominance.add(getDefaultParameter());
 		dominance.add(new RuleRankParameter(get(Labels.PARETO),"pareto", PARETO));
 		dominance.add(new RuleRankParameter(get(Labels.LORENZ),"lorenz", LORENZ));
 	}
 	
 	private void initDominanceForPairsOfOrdinalValues() {
-		dominanceForPairsOfOrdinalValues = new ArrayList<>();
+		dominanceForPairsOfOrdinalValues = new LinkedList<>();
 		dominanceForPairsOfOrdinalValues.add(getDefaultParameter());
 		dominanceForPairsOfOrdinalValues.add(new RuleRankParameter(get(Labels.STRICT_ORDINAL_DOMINANCE_CHECK_METHOD),"strict", STRICT_ORDINAL_DOMINANCE_CHECK_METHOD));
 		dominanceForPairsOfOrdinalValues.add(new RuleRankParameter(get(Labels.CLASSIC_ORDINAL_DOMINANCE_CHECK_METHOD),"classic", CLASSIC_ORDINAL_DOMINANCE_CHECK_METHOD));
 	}
 	
 	private void initNegativeExamplesTreatmentForVCDRSA() {
-		negativeExamplesTreatmentForVCDRSA = new ArrayList<>();
+		negativeExamplesTreatmentForVCDRSA = new LinkedList<>();
 		negativeExamplesTreatmentForVCDRSA.add(getDefaultParameter());
 		negativeExamplesTreatmentForVCDRSA.add(new RuleRankParameter(get(Labels.ONLY_INCONSISTENT_NEGATIVE_EXAMPLES),"only-inconsistent", COVER_ONLY_INCONSISTENT_NEGATIVE_EXAMPLES));
 		negativeExamplesTreatmentForVCDRSA.add(new RuleRankParameter(get(Labels.ONLY_INCONSISTENT_AND_BOUNDARY_NEGATIVE_EXAMPLES),"only-inconsistent-and-boundary", COVER_ONLY_INCONSISTENT_AND_BOUNDARY_NEGATIVE_EXAMPLES));
@@ -225,21 +225,21 @@ class RuleRankParametersService {
 	}
 	
 	private void initRuleConditionsSelectionMethodInVCDomLEM() {
-		ruleConditionsSelectionMethodInVCDomLEM = new ArrayList<>();
+		ruleConditionsSelectionMethodInVCDomLEM = new LinkedList<>();
 		ruleConditionsSelectionMethodInVCDomLEM.add(getDefaultParameter());
 		ruleConditionsSelectionMethodInVCDomLEM.add(new RuleRankParameter(get(Labels.CHOOSE_CONDITIONS_FROM_ONE_OBJECT),"base", CHOOSE_CONDITIONS_FROM_ONE_OBJECT));
 		ruleConditionsSelectionMethodInVCDomLEM.add(new RuleRankParameter(get(Labels.MIX_CONDITIONS_FROM_DIFFERENT_OBJECTS),"mix", MIX_CONDITIONS_FROM_DIFFERENT_OBJECTS));
 	}
 	
 	private void initOptimizeRuleConsistencyInVCDomLEMWrt() {
-		optimizeRuleConsistencyInVCDomLEMWrt = new ArrayList<>();
+		optimizeRuleConsistencyInVCDomLEMWrt = new LinkedList<>();
 		optimizeRuleConsistencyInVCDomLEMWrt.add(getDefaultParameter());
 		optimizeRuleConsistencyInVCDomLEMWrt.add(new RuleRankParameter(get(Labels.CONSISTENCY_APPROXIMATION),"approximation", APPROXIMATION));
 		optimizeRuleConsistencyInVCDomLEMWrt.add(new RuleRankParameter(get(Labels.CONSISTENCY_SET),"set", SET));
 	}
 	
 	private void initBooleanParameter() {
-		booleanParameter = new ArrayList<>();
+		booleanParameter = new LinkedList<>();
 		booleanParameter.add(getDefaultParameter());
 		booleanParameter.add(new RuleRankParameter(get(Labels.BOOLEAN_YES),"true", Integer.MAX_VALUE));
 		booleanParameter.add(new RuleRankParameter(get(Labels.BOOLEAN_NO),"false", Integer.MAX_VALUE));
